df5b7555a4b32084d57f7fd651a94b3d6ccad0b8
JFonseca-BC <jfonseca10@my.bcit.ca>
2025-05-12 15:32:30 -0700

    hotfix didn't work, reverting

--- index.js
+++ index.js
@@ -38,10 +38,7 @@ const client = new MongoClient(mongoUri, {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    },
-    tls: true,
-    tlsAllowInvalidCertificates: false,
-    tlsInsecure: false
+    }
 });
 
 let db;
